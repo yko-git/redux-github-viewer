b167b99f2be9fc312c393ec0adbb0be27bfc2cc9
yko-git <yko.iwata@gmail.com>
2024-04-21 18:39:36 +0900

    fix develop (jsconfig追加検証中)

--- src/components/atoms/LogoLinks/index.js
+++ src/components/atoms/LogoLinks/index.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const LogoLinksList = styled.ul`
@@ -15,10 +14,10 @@ export default function LogoLinks() {
   return (
     <LogoLinksList>
       <LogoLinksItem>
-        <Link to="issue">Issue</Link>
+        <a herf="/profile/">Issue</a>
       </LogoLinksItem>
       <LogoLinksItem>
-        <Link to="pullrequest">Pull Request</Link>
+        <a herf="/">Pull Request</a>
       </LogoLinksItem>
     </LogoLinksList>
   );
